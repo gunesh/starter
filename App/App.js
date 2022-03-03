d8a7a427a56a3c83d949ae5228acdfdfb86eeda9
gunesh <gunesh26@gmail.com>
2022-03-03 13:48:07 +0530

    1 file modified

--- App/App.js
+++ App/App.js
@@ -1,5 +1,11 @@
 import React, { useEffect } from 'react';
-import { Router, Route, Switch, Redirect } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Outlet,
+  Navigate,
+} from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { history } from '../_helpers';
@@ -9,36 +15,61 @@ import { HomePage } from '../HomePage';
 import { LoginPage } from '../LoginPage';
 import { RegisterPage } from '../RegisterPage';
 
-function App() {
-    const alert = useSelector(state => state.alert);
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        history.listen((location, action) => {
-            // clear alert on location change
-            dispatch(alertActions.clear());
-        });
-    }, []);
-
-    return (
-        <div className="jumbotron">
-            <div className="container">
-                <div className="col-md-8 offset-md-2">
-                    {alert.message &&
-                        <div className={`alert ${alert.type}`}>{alert.message}</div>
-                    }
-                    <Router history={history}>
-                        <Switch>
-                            <PrivateRoute exact path="/" component={HomePage} />
-                            <Route path="/login" component={LoginPage} />
-                            <Route path="/register" component={RegisterPage} />
-                            <Redirect from="*" to="/" />
-                        </Switch>
-                    </Router>
-                </div>
-            </div>
-        </div>
-    );
+function PrivateOutlet(props) {
+  return localStorage.getItem('user') ? (
+    <>
+      {console.log('Im ')}
+      <Outlet />
+      {props.Children}
+    </>
+  ) : (
+    <>
+      {console.log('You r')}
+      <Navigate to="/login" />
+    </>
+  );
 }
 
-export { App }
+function App() {
+  const alert = useSelector((state) => state.alert);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    history.listen((location, action) => {
+      // clear alert on location change
+      dispatch(alertActions.clear());
+    });
+  }, []);
+
+  return (
+    <div className="jumbotron">
+      <div className="container">
+        <div className="col-md-8 offset-md-2">
+          {alert.message && (
+            <div className={`alert ${alert.type}`}>{alert.message}</div>
+          )}
+          {/* <Router history={history}>
+            <Switch>
+              <PrivateRoute exact path="/" component={HomePage} />
+              <Route path="/login" component={LoginPage} />
+              <Route path="/register" component={RegisterPage} />
+              <Redirect from="*" to="/" />
+            </Switch>
+          </Router> */}
+
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<PrivateOutlet />}>
+                <Route element={<HomePage />} />
+              </Route>
+              <Route path="login" element={<LoginPage />} />
+              <Route path="register" element={<RegisterPage />} />
+            </Routes>
+          </BrowserRouter>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export { App };
